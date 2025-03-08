634c077060e69620fc06cbb11f75db1d50768258
Benyouceftebbi <benyoucef.tebbi@std.antalya.edu.tr>
2025-03-08 01:16:36 +0100

    new

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -4,12 +4,12 @@ import {getFirestore} from 'firebase/firestore'
 import { getStorage } from "firebase/storage";
 import { connectFunctionsEmulator, getFunctions } from 'firebase/functions';
 const firebaseConfig = {
-  apiKey: "AIzaSyA4YsEHLisv0iblWmyH7qE3CQaYv3d-ClI",
-  authDomain: "deliverysync-9fdcd.firebaseapp.com",
-  projectId: "deliverysync-9fdcd",
-  storageBucket: "deliverysync-9fdcd.firebasestorage.app",
-  messagingSenderId: "907163553140",
-  appId: "1:907163553140:web:48d0127a7a4b3b1b353021"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
